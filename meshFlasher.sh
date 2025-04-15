843e892e9009398f0ac6dd8f12e2321f7d1bfe0b
Luke Switzer <10099969+lukeswitz@users.noreply.github.com>
2025-04-15 04:42:36 -0600

    Update meshFlasher.sh

--- meshFlasher.sh
+++ meshFlasher.sh
@@ -4,7 +4,6 @@ set -e
 # Variables
 ESPTOOL_REPO="https://github.com/alphafox02/esptool"
 FIRMWARE_OPTIONS=(
-    
     "ESP32C3 - Camera Detector:https://github.com/lukeswitz/mesh-detect/raw/refs/heads/main/dist/oui-detection/meshdetect_privacy.bin"
     "ESP32C3 - Deepwoods Device Baseline Alert:https://github.com/lukeswitz/mesh-detect/raw/refs/heads/main/dist/deepwoods/esp32c3_device_fingerprint.bin"
     "ESP32C3 - WiFi Drone RID:https://github.com/colonelpanichacks/WiFi-RemoteID-to-mesh/raw/refs/heads/main/firmware.bin"
