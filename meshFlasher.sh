b79cca65aa4f9cdb56532e0c7d196043e236d48f
Luke Switzer <10099969+lukeswitz@users.noreply.github.com>
2025-04-17 17:59:34 -0600

    use main node mode bin

--- meshFlasher.sh
+++ meshFlasher.sh
@@ -7,7 +7,7 @@ FIRMWARE_OPTIONS=(
     "C3 - Camera Detector:https://github.com/lukeswitz/mesh-detect/raw/refs/heads/main/dist/oui-detection/meshdetect_privacy.bin"
     "C3 - Deepwoods Device Baseline Alert:https://github.com/lukeswitz/mesh-detect/raw/refs/heads/main/dist/deepwoods/esp32c3_device_fingerprint.bin"
     "C3 - WiFi Drone RID:https://github.com/colonelpanichacks/WiFi-RemoteID-to-mesh/raw/refs/heads/main/firmware.bin"
-    "C3 - WiFi Drone RID Node Mode:https://github.com/lukeswitz/WiFi-RemoteID-to-mesh/raw/refs/heads/main/node-mode/firmware.bin"
+    "C3 - WiFi Drone RID Node Mode:https://github.com/colonelpanichacks/WiFi-RemoteID-to-mesh/raw/refs/heads/main/node-mode/firmware.bin"
     "S3 - WiFi & BT Drone RID:https://github.com/lukeswitz/BLE-RemoteID-to-mesh/raw/refs/heads/dualcore/dual_firmware_s3.bin"
     "S3 - WiFi & BT Drone RID Node Mode:https://github.com/lukeswitz/WiFi-RemoteID-to-mesh/raw/refs/heads/main/node-mode%20dualcore/firmware.bin"
     "S3 - BT Drone RID:https://github.com/lukeswitz/mesh-detect/raw/refs/heads/main/dist/drone-detect/dji_ble_firmware_s3.bin"
