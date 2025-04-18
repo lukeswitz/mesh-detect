c046ad74e9fa3a675a375ee13a2b7566e4967d80
Luke Switzer <10099969+lukeswitz@users.noreply.github.com>
2025-04-18 08:19:54 -0600

    add bt drone builds

--- meshFlasher.sh
+++ meshFlasher.sh
@@ -8,9 +8,10 @@ FIRMWARE_OPTIONS=(
     "C3 - Deepwoods Device Baseline Alert:https://github.com/lukeswitz/mesh-detect/raw/refs/heads/main/dist/deepwoods/esp32c3_device_fingerprint.bin"
     "C3 - WiFi Drone RID:https://github.com/colonelpanichacks/WiFi-RemoteID-to-mesh/raw/refs/heads/main/firmware.bin"
     "C3 - WiFi Drone RID Node Mode:https://github.com/colonelpanichacks/WiFi-RemoteID-to-mesh/raw/refs/heads/main/node-mode/firmware.bin"
+    "C3 - BT Drone RID:https://github.com/colonelpanichacks/BLE-RemoteID-to-mesh/raw/refs/heads/main/firmware.bin"
+    "C3 - BT Drone RID Node Mode:https://github.com/colonelpanichacks/BLE-RemoteID-to-mesh/raw/refs/heads/main/node-mode/firmware.bin"
     "S3 - WiFi & BT Drone RID:https://github.com/lukeswitz/BLE-RemoteID-to-mesh/raw/refs/heads/dualcore/dual_firmware_s3.bin"
     "S3 - WiFi & BT Drone RID Node Mode:https://github.com/lukeswitz/WiFi-RemoteID-to-mesh/raw/refs/heads/main/node-mode%20dualcore/firmware.bin"
-    "S3 - BT Drone RID:https://github.com/lukeswitz/mesh-detect/raw/refs/heads/main/dist/drone-detect/dji_ble_firmware_s3.bin"
     "C6 - BT Drone RID:https://github.com/colonelpanichacks/BLE-RemoteID-to-mesh/raw/refs/heads/main/firmware.bin"
 )
 ESPTOOL_DIR="esptool"
@@ -65,7 +66,7 @@ fi
 
 echo ""
 echo "==================================================="
-echo "Available firmware options:"
+echo "Available firmware options for esp32c3/c6/s3:"
 echo "==================================================="
 
 declare -a options_array
